9860d88481d671911010e0656decf6e1941f5380
Architect <solution.architect@scalable.software>
2025-12-25 10:03:52 +0100

    EVENT: `Event.ON` Exists -> FAIL

--- test/unit/pin.test.ts
+++ test/unit/pin.test.ts
@@ -512,6 +512,37 @@ operation(Operation.TOGGLE, () => {
   });
 });
 
+events(Event.ON, () => {
+  and("Pin is defined in custom element registry", () => {
+    beforeEach(() => {
+      define(Pin.Tag, Pin);
+    });
+
+    and("HTML Template is added to DOM", () => {
+      beforeEach(async () => {
+        await Pin.Template.load("pin.template.html");
+      });
+      afterEach(() => {
+        remove(Pin.Tag);
+      });
+
+      and("a new pin is added to DOM", () => {
+        let pin: Pin;
+        beforeEach(() => {
+          pin = add<Pin>(Pin.Tag);
+        });
+        afterEach(() => {
+          pin.remove();
+        });
+
+        then("pin.on setter is defined", () => {
+          expect(hasSetter(pin, Event.ON)).toBeTrue();
+        });
+      });
+    });
+  });
+});
+
 events(Event.ON_HIDE, () => {
   and("Pin is defined in custom element registry", () => {
     beforeEach(() => {
