014e33f592e75dde4193a77891968a82946a3856
Architect <solution.architect@scalable.software>
2025-12-25 09:22:49 +0100

    EVENT: `pin.onpin` setter Exists -> FAIL

--- test/unit/pin.test.ts
+++ test/unit/pin.test.ts
@@ -650,3 +650,34 @@ events(Event.ON_SHOW, () => {
     });
   });
 });
+
+events(Event.ON_PIN, () => {
+  and("Pin is defined in custom element registry", () => {
+    beforeEach(() => {
+      define(Pin.Tag, Pin);
+    });
+
+    and("HTML Template is added to DOM", () => {
+      beforeEach(async () => {
+        await Pin.Template.load("pin.template.html");
+      });
+      afterEach(() => {
+        remove(Pin.Tag);
+      });
+
+      and("a new pin is added to DOM", () => {
+        let pin: Pin;
+        beforeEach(() => {
+          pin = add<Pin>(Pin.Tag);
+        });
+        afterEach(() => {
+          pin.remove();
+        });
+
+        then("pin.onpin setter is defined", () => {
+          expect(hasSetter(pin, Event.ON_PIN)).toBeTrue();
+        });
+      });
+    });
+  });
+});
