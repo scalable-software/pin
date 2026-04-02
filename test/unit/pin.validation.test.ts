a16ae4b1d4ab73040736044ca4d20f16db9930d3
Architect <solution.architect@scalable.software>
2026-04-02 18:10:39 +0200

    VALIDATION: Validate.visible(value) Throws error if value is not in Visible -> FAIL

--- test/unit/pin.validation.test.ts
+++ test/unit/pin.validation.test.ts
@@ -1,1 +1,57 @@
-import { State } from "@scalable.software/pin";
+import { State, Visible } from "@scalable.software/pin";
+
+import { Validate } from "@scalable.software/pin";
+
+validation(State.VISIBLE, () => {
+  given("Validate imported", () => {
+    and("value is a valid visible state", () => {
+      let value: any;
+      beforeEach(() => {
+        value = Visible.YES;
+      });
+      when("Validate.visible(value) is called", () => {
+        let error;
+        let result;
+        beforeEach(() => {
+          try {
+            result = Validate.visible(value);
+          } catch (err) {
+            error = err;
+          }
+        });
+        then("does not throw an error", () => {
+          expect(error).toBeUndefined();
+        });
+      });
+    });
+
+    and("value is an invalid visible state", () => {
+      let value: any;
+      beforeEach(() => {
+        value = "invalid";
+      });
+      when("Validate.visible(value) is called", () => {
+        let error;
+        let result;
+        beforeEach(() => {
+          try {
+            result = Validate.visible(value);
+          } catch (err) {
+            error = err;
+          }
+        });
+        then("Validate.visible(value) throws", () => {
+          expect(error).not.toBeUndefined();
+        });
+
+        and("error message contains 'Invalid visible value'", () => {
+          then("error message contains 'Invalid visible value'", () => {
+            expect((error as Error).message).toEqual(
+              "Invalid visible value: invalid",
+            );
+          });
+        });
+      });
+    });
+  });
+});
